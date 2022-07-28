4b61860060b497c4818b81c8305c112c7bd0892c
jamaps <jeffallen89@gmail.com>
2022-07-28 14:32:25 -0400

    vancouver data prep and map

--- data/vancouver/prep-zones.py
+++ data/vancouver/prep-zones.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import geopandas as gpd
 
+# just the CD zones
+
 union = gpd.read_file("data/zoning-districts-CD-LU-union.shp")
 
 union = union[["object_id","Descriptio"]].sort_values("object_id")
@@ -23,7 +25,7 @@ union["LU_z"] = union["Descriptio"].apply(assign_type)
 
 dfcd = pd.DataFrame(union.groupby('object_id')["LU_z"].max())
 
-def assign_zone(LU_z):
+def assign_zone_from_LUz(LU_z):
 	if LU_z == 3:
 		zone_type = "MU"
 	elif LU_z == 2:
@@ -31,13 +33,42 @@ def assign_zone(LU_z):
 	elif LU_z == 1:
 		zone_type = "SD"
 	else:
-		zone_type = None
+		zone_type = "None"
 	return zone_type
 
-dfcd["zone_type"] = dfcd["LU_z"].apply(assign_zone)
+dfcd["zone_type"] = dfcd["LU_z"].apply(assign_zone_from_LUz)
 
-# dfcd.to_csv("zoning-districts-CD-categorized.csv")
+dfcd.index.name = 'object_id'
+dfcd.reset_index(inplace=True)
+dfcd = dfcd[["object_id","zone_type"]]
 
-print(dfcd["object_id","zone_type"])
 
-dfcd[""]
+# all other zones 
+
+zones = gpd.read_file("data/zoning-districts-and-labels.shp")
+
+dfz = zones[zones["zoning_clas"] != "Comprehensive Development"]
+
+def assign_zone_from_districts(district):
+	if district in ["RS","RA","FSHCA"]:
+		zone_type = "SD"
+	elif district in ["RT","RM","FM"]:
+		zone_type = "OR"
+	elif district in ["HA","FC","C","DD","DEOD","FCCDD"]:
+		zone_type = "MU"
+	else:
+		zone_type = "None"
+	return zone_type
+
+dfz["zone_type"] = dfz["zoning_cate"].apply(assign_zone_from_districts)
+
+dfz = dfz[["object_id","zone_type"]]
+
+
+# combine the two 
+
+df = pd.concat([dfcd, dfz], axis=0)
+
+print(df)
+
+df.to_csv("data/zones_with_categories.csv")
