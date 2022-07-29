ba4ae2f05532e5e3ccbd00c2f5548c61af6a1e2b
jamaps <jeffallen89@gmail.com>
2022-07-28 21:20:07 -0400

    update legends on print maps

--- src/App.svelte
+++ src/App.svelte
@@ -196,16 +196,19 @@
 
 	<div class="info">
 		<p>
-			<b>More Information:</b>	<a href="https://www.toronto.ca/zoning/bylaw_amendments/ZBL_NewProvision_Chapter1.htm">Zoning By-Law</a> | <a href="https://open.toronto.ca/dataset/zoning-by-law/">Spatial Data</a> | <a href="">High Resolution Map</a>
+			<b>More Information:</b> <a href="https://vancouver.ca/home-property-development/zoning-and-land-use-policies-document-library.aspx">Zoning By-Law</a> | <a href="https://opendata.vancouver.ca/explore/dataset/zoning-districts-and-labels/information/?disjunctive.zoning_classification&disjunctive.zoning_category&disjunctive.zoning_district">Spatial Data</a> | <a href="">High Resolution Map</a>
 		</p>
 	</div>
 
 	<div class="categories">
 		<code>
-			<b>Single Detached Zones:</b><br> RD <br>
-			<b>Other Residential Zones:</b><br> R, RA, RM, RS, RS <br>
-			<b>Mixed Use Zones:</b><br> RAC, CR, CRE
+			<b>Single Detached Zones:</b><br> RA, RS, FSHCA <br>
+			<b>Other Residential Zones:</b><br> RT, RM, FM <br>
+			<b>Mixed Use Zones:</b><br> HA, FC, C, DD, DEOD, FCCDD
 		</code>
+		<p>
+			For Vancouver, the single detached only zones also allow duplexes. There were also hundres of CD-1 (Comprehensive Development) zones, each with their own specific allowances. These were categorized based on joining to Metro Vancouvers 2016 land use <a href="http://www.metrovancouver.org/data">data</a>. 
+		</p>
 	</div>
 
 
@@ -350,6 +353,9 @@
 		padding-bottom: 50px;
 	}
 
+	.categories p {
+		font-size: 13px;
+	}
 
 
 	@media (min-width: 480px) {
