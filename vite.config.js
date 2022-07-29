d21649491239f8b024ca258ee7224bbf7faf85e7
jamaps <jeffallen89@gmail.com>
2022-07-29 13:31:43 -0400

    build dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/yellowbelt-exclusionary-zoning-canadian-cities",
+	base: "/yellowbelt-canadian-cities-2022",
 	plugins: [svelte()]
 })
